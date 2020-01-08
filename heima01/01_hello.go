70c4edfec2f618086dc253d427dc19696813d5ab
lianxiang <1161851688@qq.com>
2020-01-09 07:27:50 +0800

    基础语法

---
+++ heima01/01_hello.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("爱白菜的小昆虫")
+}
---
+++ heima01/02_hello.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("爱白菜的小昆虫")
+	fmt.Println("word")
+}
