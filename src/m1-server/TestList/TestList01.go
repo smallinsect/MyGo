d1bfbd3a4636df59a504b6137879fe95b199b182
maichi <513312514@qq.com>
2020-02-20 10:33:22 +0800

    添加list测试代码

---
+++ src/m1-server/TestList/TestList01.go
@@ -0,0 +1,13 @@
+package testchannel
+
+import (
+	"container/list"
+	"fmt"
+)
+
+//ChannelFunc ChannelFunc
+func ListFunc() {
+	var l list.List
+	var e list.Element
+
+}
---
+++ src/m1-server/TestList/TestList01_test.go
@@ -0,0 +1,8 @@
+package testchannel
+
+import "testing"
+
+func TestListFunc(t *testing.T) {
+	ListFunc()
+	t.Logf("test ChannelFunc succ")
+}
