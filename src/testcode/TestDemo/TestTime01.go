e79734a454825806e65640f4c9c9b4dcc08ff73a
maichi <513312514@qq.com>
2020-03-14 15:18:36 +0800

    时间

--- src/testcode/TestDemo/TestTime01.go
+++ src/testcode/TestDemo/TestTime01.go
@@ -17,8 +17,8 @@ func TimeFunc() {
 
 //TimeFunc01 TimeFunc01
 func TimeFunc01() {
-	t := int64(1546926630)      //外部传入的时间戳（秒为单位），必须为int64类型
-	t1 := "2020-02-20 18:41:00" //外部传入的时间字符串
+	t := int64(1585118075)      //外部传入的时间戳（秒为单位），必须为int64类型
+	t1 := "2020-03-25 14:34:35" //外部传入的时间字符串
 
 	//时间转换的模板，golang里面只能是 "2006-01-02 15:04:05" （go的诞生时间）
 	timeTemplate1 := "2006-01-02 15:04:05" //常规类型
@@ -33,8 +33,11 @@ func TimeFunc01() {
 	fmt.Println(time.Unix(t, 0).Format(timeTemplate4)) //输出：13:50:30
 
 	// ======= 将时间字符串转换为时间戳 =======
-	stamp, _ := time.ParseInLocation(timeTemplate1, t1, time.Local) //使用parseInLocation将字符串格式化返回本地时区时间
-	fmt.Println(stamp.Unix())                                       //输出：1546926630
+	stamp, err := time.ParseInLocation(timeTemplate1, t1, time.Local) //使用parseInLocation将字符串格式化返回本地时区时间
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(stamp.Unix()) //输出：1546926630
 }
 
 //TimeFunc02 当前时间加1个小时
--- src/testcode/TestDemo/TestTime01_test.go
+++ src/testcode/TestDemo/TestTime01_test.go
@@ -3,6 +3,6 @@ package testdemo
 import "testing"
 
 func TestTimeFunc(t *testing.T) {
-	TimeFunc02()
+	TimeFunc01()
 	t.Logf("test Add succ")
 }
