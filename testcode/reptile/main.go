f658c05826ef5f1ff309d7cc2ff4fb21fb931892
maichi <513312514@qq.com>
2020-06-01 10:21:57 +0800

    新增代码

--- testcode/reptile/main.go
+++ testcode/reptile/main.go
@@ -2,14 +2,28 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
 )
 
 func main() {
 	fmt.Println("heheh")
-	reps, err := http.Get("")
+	reps, err := http.Get("https://www.runoob.com/linux/linux-vim.html")
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
+	defer reps.Body.Close()
+
+	if reps.StatusCode != http.StatusOK {
+		fmt.Println("", reps.StatusCode)
+		return
+	}
+
+	result, err := ioutil.ReadAll(reps.Body)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("%s", result)
 }
